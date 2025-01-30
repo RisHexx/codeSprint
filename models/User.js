23252a68f0795cdf1d8802ae8d51256be137ed3c
AtharvaGhorpade2405 <atharvasg24@gmail.com>
2025-01-30 12:10:45 +0530

    hello

--- models/User.js
+++ models/User.js
@@ -4,9 +4,9 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['citizen', 'municipal_officer'], required: true },
-    age: { type: Number, required: true },
-    gender: { type: String, enum: ['male', 'female', 'other'], required: true }
+    // role: { type: String, enum: ['citizen', 'municipal_officer'], required: true },
+    // age: { type: Number, required: true },
+    // gender: { type: String, enum: ['male', 'female', 'other'], required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', UserSchema);
